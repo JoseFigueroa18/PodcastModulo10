02ebace90eaab3ba2d6fe65e0eb834f800c8998c
José <josehernanfigueroa18@gmail.com>
2025-04-08 17:29:42 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensaje() {
+    const mensaje = document.getElementById('mensaje');
+    mensaje.textContent = "Gracias por escuchar. Creador: José Hernán Figueroa";
+    mensaje.style.display = "block";
+}
